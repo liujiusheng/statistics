c212d47be9a5b7c6c6a51b4d5a08ad71285cba93
liujiusheng <891599396@qq.com>
2016-12-02 14:12:22 +0800

    完成求和、平均值、最大值、最小值函数

--- src/array.js
+++ src/array.js
@@ -3,27 +3,45 @@
  *---------------------------------------------------
  */
  
+ 
  //求和
  function count(array){
-	
+	var count = 0;
+	for(var i in array){
+		count += array[i];
+	}
+	return count;
  }
  
  
  //求平均值
  function avg(array){
-	 
+	var count = count(array);
+	return count/array.length;
  }
  
  
  //求最大值
  function max(array){
-	 
+	 var max = array[0];
+	 for(var i in array){
+		if(array[i] > max){
+			max = array[i];
+		} 
+	 }
+	 return max;
  }
  
  
  //最小值
  function min(array){
-	 
+	var min = array[0];
+	 for(var i in array){
+		if(array[i] < min){
+			min = array[i];
+		} 
+	 }
+	 return min; 
  }
  
  
