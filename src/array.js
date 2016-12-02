cf10ee12edfcd73e819078c59b94b2b3b9212202
liujiusheng <891599396@qq.com>
2016-12-02 10:08:18 +0800

    新增数组统计函数

---
+++ src/array.js
@@ -0,0 +1,29 @@
+/**
+ * 数组
+ *---------------------------------------------------
+ */
+ 
+ //求和
+ function count(array){
+	
+ }
+ 
+ 
+ //求平均值
+ function avg(array){
+	 
+ }
+ 
+ 
+ //求最大值
+ function max(array){
+	 
+ }
+ 
+ 
+ //最小值
+ function min(array){
+	 
+ }
+ 
+ 
